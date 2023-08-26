08c8d248e8ca39ff991be2e32b56da2187f3d8b1
xiaogaostudent <2223405546@qq.com>
2023-08-26 05:18:05 -0700

    nothing

---
+++ UI/main.py
@@ -0,0 +1,34 @@
+from PyQt5 import QtWidgets, uic
+import sys
+
+from PyQt5.QtWidgets import QMainWindow, QDesktopWidget
+
+from logindialog import Ui_LoginDialog
+from qt_material import apply_stylesheet
+
+class Ui(QMainWindow, Ui_LoginDialog):
+    def __init__(self, parent = None):
+        super(Ui, self).__init__(parent)
+        self.setupUi(self)
+
+    def center(self):  # 定义一个函数使得窗口居中显示
+        # 获取屏幕坐标系
+        screen = QDesktopWidget().screenGeometry()
+        # 获取窗口坐标系
+        size = self.geometry()
+        newLeft = (screen.width() - size.width()) / 2
+        newTop = (screen.height() - size.height()) / 2
+        self.move(int(newLeft), int(newTop))
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+window.center()
+#设置对象名称
+window.setObjectName("MainWindow")
+
+window.setStyleSheet("#MainWindow{border-image:url(./picture/kun.gif);}")
+
+#window.setStyleSheet("#MainWindow{background-color: yellow}")
+apply_stylesheet(app, theme='light_pink.xml')
+window.show()
+sys.exit(app.exec())
